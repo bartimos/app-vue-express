9c9e2ea3b3cdf00d9e77db9bde8c913574bcbde8
Unknown <unknown@example.com>
2019-11-26 23:32:21 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # app-vue-express
 my first app with vue and express
+
+express middleware:
+body-parse  解析请求
+cors  跨域问题
+morgan 
+nodemon 热重启
+
+npm i -D nodemon eslint 安装开发依赖
---
+++ app-front/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  methods:{
+    init(){
+      console.log('test')
+    },
+    getData(){
+      // var url="http://localhost:3000"
+      // this.$ajax.get('http://localhost:3000/users').then(function (response) {
+      //   console.log(response);
+      // }).catch(function (error) { 
+      //   console.log(error); 
+      // })
+      this.$ajax.get('/users').then((response)=>{
+        console.log(response)
+      }).catch((response)=>{
+        console.log(response);
+      })
+      
+    }
+  },
+  mounted(){
+    this.init()
+    this.getData()    
+  }
+}
+</script>
